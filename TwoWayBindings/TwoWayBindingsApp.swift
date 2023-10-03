33869c740da400004dfc802a11b6ac0eb267ffd4
David Veeneman <davidv@veeneman.com>
2023-10-03 10:56:30 -0700

    Initial Commit

---
+++ TwoWayBindings/TwoWayBindingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwoWayBindingsApp.swift
+//  TwoWayBindings
+//
+//  Created by David Veeneman on 10/3/23.
+//
+
+import SwiftUI
+
+@main
+struct TwoWayBindingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
