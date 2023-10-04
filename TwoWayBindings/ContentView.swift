399e4407410b48de30a1504976a9bf04cb422b0b
David Veeneman <davidv@veeneman.com>
2023-10-03 18:07:02 -0700

    2023-10-03: End of Chapter 1.21. Examples of binding to a variety ov control views.

--- TwoWayBindings/ContentView.swift
+++ TwoWayBindings/ContentView.swift
@@ -8,16 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var pressCount = 0
+    @State private var toggleIsOn = true
+    @State private var name = ""
+    @State private var selectedColor: Color = .red
+    @State private var selectedDate = Date()
+    @State private var stepperValue = 1
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            
+            // Button Control
+            Text("Press Count: \(pressCount)")
+                .font(.title)
+            Button("Press Me"){
+                pressCount += 1
+            }
+            .buttonStyle(.borderedProminent)
+            
+            Spacer()
+            
+            // Text Field Control
+            TextField("Enter a name", text: $name)
+                .textFieldStyle(.roundedBorder)
+            Text("Text entered: \(name)")
+           
+            Spacer()
+            
+            // Toggle Control
+            Toggle("Toggle is \(toggleIsOn ? "on" : "off")", isOn: $toggleIsOn)
+            
+            Spacer()
+            
+            // Color Picker
+            Rectangle()
+                .fill(selectedColor)
+                .frame(width: 100, height: 100)
+            ColorPicker("Pick a rectangle color", selection: $selectedColor)
+            
+            Spacer()
+            
+           // Date Picker
+            DatePicker("Date:", selection: $selectedDate)
+            Text("Selected date is: \(selectedDate.formatted(date: .abbreviated, time: .shortened))")
+            
+            Spacer()
+            
+            Stepper("Stepper value: \(stepperValue)", value: $stepperValue, in: 1...10)
+            Spacer()
+            
         }
+        .font(.title2)
         .padding()
     }
 }
+
 
 #Preview {
     ContentView()
